df3ee68b707cf8da08d722875072c04b996ba4f4
polykernel <81340136+polykernel@users.noreply.github.com>
2023-04-20 21:34:39 -0400

    feat(pkgs): add wlopm and lswt
    - Init wlopm at master
    - Init lswt at master

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -7,4 +7,6 @@ rec {
   wired-notify = pkgs.callPackage ./wired-notify {};
   swayimg = pkgs.callPackage ./swayimg {};
   wldash = pkgs.callPackage ./wldash {};
+  wlopm = pkgs.callPackage ./wlopm {};
+  lswt = pkgs.callPackage ./lswt {};
 }
