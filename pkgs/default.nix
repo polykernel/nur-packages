efc9aa446832299aa421488a18f201241218e38b
polykernel <81340136+polykernel@users.noreply.github.com>
2023-12-17 12:40:22 -0500

    chore(pkgs): remove packages
    - Remove the following packages as they are now avaliable (and maintained in better quality)
    in Nixpkgs
      - kickoff
      - swayimg
      - kile(kile-wl)
      - lorien
      - lswt
      - wlopm

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,12 +1,6 @@
 {pkgs ? import <nixpkgs> {}}:
 
 rec {
-  lorien = pkgs.callPackage ./lorien {};
-  kickoff = pkgs.callPackage ./kickoff {};
   wired-notify = pkgs.callPackage ./wired-notify {};
-  swayimg = pkgs.callPackage ./swayimg {};
   wldash = pkgs.callPackage ./wldash {};
-  wlopm = pkgs.callPackage ./wlopm {};
-  lswt = pkgs.callPackage ./lswt {};
-  kile = pkgs.callPackage ./kile {};
 }
